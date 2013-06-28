9b77c3320d285a788fc99b32f551d5c70b3c181f
Mike Mackintosh <m@zyp.io>
2013-06-28 15:06:54 -0400

    Removed specific SQL drivers, supports only PDO and fixed bug with URLGenerator and arguments

---
+++ Pantry/Libraries/URLGenerator/URLGenerator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bakery\Pantry\Libraries\URLGenerator;
+
+class URLGenerator{
+	
+	public function __construct(){
+		//echo __METHOD__;
+	}
+
+	/**
+	 * generate a url
+	 * 
+	 * @param  [string] $route [description]
+	 * @param  [array] $args  [description]
+	 * @return [string]        [description]
+	 */
+	public function generate( $route, $args = [] ){
+
+
+		foreach( \Bakery::$cookbook as $cbroute => $recipe ){
+			if( $recipe->getRouteName() == $route){
+
+				if( !empty( $recipe->_uriPattern )){
+					
+					$i = 0;
+					foreach( $recipe->_uriPattern as $k => $v ){
+
+						$cbroute = str_replace("{{$k}}", $args[$i++], $cbroute);
+
+					}
+
+				}
+				
+				return $cbroute;
+
+			}
+
+		}
+
+	}
+
+}
